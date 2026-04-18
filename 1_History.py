58d6ed2809a430a53ea0bff9dddf31e178cae84f
vineet29work-lab <vineet.29.work@gmail.com>
2026-04-18 17:04:05 +0530

    Delete 1_History.py

--- 1_History.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.header("Peek into your chat history...")
-if "chat_history" in st.session_state:
-    for msg in st.session_state.chat_history:
-        with st.chat_message(msg["role"]):
-            st.write(msg["content"])
