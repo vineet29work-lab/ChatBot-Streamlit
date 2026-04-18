af56cf3b6f1aba42573f39ce7c70686b375f07a4
vineet29work-lab <vineet.29.work@gmail.com>
2026-04-18 17:17:08 +0530

    Update 1_History.py

--- pages/1_History.py
+++ pages/1_History.py
@@ -9,4 +9,3 @@ if "chat_history" in st.session_state:
 with st.sidebar:
     st.title("Socials")
     st.page_link(label = "Instagram", page = "https://www.instagram.com/_k.vineettt?igsh=aXNpNzJpd2Ewb280", icon = "🔗")
-    st.page_link(label = "G-mail", page = "https://vineet.29.work@gmail.com", icon = "🔗")
