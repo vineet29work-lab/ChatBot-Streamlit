ff2d048e2a694447be8fc529b88cab1528220eeb
vineet29work-lab <vineet.29.work@gmail.com>
2026-04-18 17:08:42 +0530

    Update 1_History.py

--- pages/1_History.py
+++ pages/1_History.py
@@ -5,3 +5,8 @@ if "chat_history" in st.session_state:
     for msg in st.session_state.chat_history:
         with st.chat_message(msg["role"]):
             st.write(msg["content"])
+
+with st.sidebar:
+    st.title("Socials")
+    st.page_link(label = "Instagram", page = "https://www.instagram.com/_k.vineettt?igsh=aXNpNzJpd2Ewb280", icon = "🔗")
+    st.page_link(label = "G-mail", page = "https://vineet.29.work@gmail.com", icon = "🔗")
